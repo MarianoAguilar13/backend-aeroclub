32828a4d79e2430c2603154609f3d214bbcf8cc0
Mariano Aguilar <mariano_aguilar_13@hotmail.com>
2023-12-18 18:22:00 -0300

    Acomodando la estetica del pdf y ahora no lo guarda y elimina, sino que se guarda en memoria y desde ahi se envia al email

--- myapi/app/controllers/recibos_pdf.py
+++ myapi/app/controllers/recibos_pdf.py
@@ -4,8 +4,10 @@ from app import db
 from app.controllers.recibosVuelos import RecibosController
 from app.models.user_model import Recibos
 from dotenv import load_dotenv
-from email.message import EmailMessage
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from app.models.user_model import UsuariosTienenRecibos
 from app.models.user_model import Usuarios
 
@@ -20,37 +22,46 @@ class ReciboPDF(FPDF):
         ruta_logo = os.path.join(directorio_actual, 'logo.png')
 
         self.image(ruta_logo,10,8,33)
-        self.set_font('Arial', 'B''U', 12)
-        self.cell(0, 10, 'Recibo Aeroclub Lincoln', 0, 1, 'C')
+        self.set_font('Arial', 'B''U', 24)
+        self.cell(0, 50, 'Recibo Aeroclub Lincoln', 0, 20, 'C')
 
-    def datos_recibo(self, data, fecha):
+    def datos_recibo(self, data, fecha,numRecibo):
         # Información del recibo
-        self.set_font('Arial', '', 10)
-        self.cell(0, 10, f"Fecha: {fecha}", 0, 1)
-        self.cell(0, 10, f"- Asociado: {data['asociado']}", 0, 1)
-        self.cell(0, 10, f"- Instructor: {data['instructor']}", 0, 1)
-        self.cell(0, 10, f"- Gestor: {data['gestor']}", 0, 1)
-        self.cell(0, 10, f" -Matrícula: {data['matricula']}", 0, 1)
-        self.cell(0, 10, f"- Observaciones: {data['observaciones']}", 0, 1)
-        self.cell(0, 10, "--------------------------------------------------------------------------------------------------", 0, 1)
-        self.cell(0, 10, f"- Precio Total: {data['precioTotal']}", 0, 1)
-        self.cell(0, 10, "--------------------------------------------------------------------------------------------------", 0, 1)
-        self.cell(0, 10, '', 0, 1)
+        self.set_font('Arial', '', 14)
+        self.text(150,10,f"Fecha y Hora:")
+        self.text(150,20,f"{fecha}")
+        self.cell(190, 60, "", 1, 1)
+        self.text(11,70,f"Asociado: {data['asociado']}")
+        self.text(11,90,f"Instructor: {data['instructor']}")
+        self.text(11,110,f"Gestor: {data['gestor']}")
+        self.text(140,70,f"Número de recibo: ")
+        self.set_font('Arial', 'B', 20)
+        self.text(140,80,f" {numRecibo}")
+        self.set_font('Arial', '', 14)
+        self.text(140,90,f"Matrícula: {data['matricula']}")
+        self.text(140,100,f"Precio Total:")
+        self.set_font('Arial', 'B', 20)
+        self.text(140,110,f"{data['precioTotal']}")
+        self.set_font('Arial', '', 14)
+        self.multi_cell(0, 20, f"Observaciones: {data['observaciones']}",1,1)
+        self.cell(0, 10, '', 0, 1)  # Espacio entre itinerarios
+
+
 
     def itinerarios(self, itinerarios):
         # Detalles de los itinerarios
-        self.set_font('Arial', 'B', 10)
-        self.cell(0, 10, 'Itinerarios', 0, 1)
-
-        self.set_font('Arial', '', 10)
+        count = 0
         for itinerario in itinerarios:
-            self.cell(0, 10, f"Hora de Salida: {itinerario['horaSalida']}", 0, 1)
-            self.cell(0, 10, f"Código Aero. Salida: {itinerario['codAeroSalida']}", 0, 1)
-            self.cell(0, 10, f"Hora de Llegada: {itinerario['horaLlegada']}", 0, 1)
-            self.cell(0, 10, f"Código Aero. Llegada: {itinerario['codAeroLlegada']}", 0, 1)
-            self.cell(0, 10, f"Cantidad de Aterrizajes: {itinerario['cantAterrizajes']}", 0, 1)
-            self.cell(0, 10, f"Tipo de Itinerario: {itinerario['tipoItinerario']}", 0, 1)
+            count = count + 1
+            self.set_font('Arial', 'B', 20)
+            self.cell(0, 10, f'{count}° Itinerario', 0, 1)
+            self.cell(0, 5, '', 0, 1)  # Espacio entre itinerarios
+            self.set_font('Arial', '', 14)
+            self.cell(0, 20, f"Hora de Salida: {itinerario['horaSalida']} - Código de aeropuerto de salida: {itinerario['codAeroSalida']}", 1, 1)
+            self.cell(0, 20, f"Hora de Llegada: {itinerario['horaLlegada']} - Código de aeropuerto de llegada: {itinerario['codAeroLlegada']}", 1, 1)
+            self.cell(0, 20, f"Tipo de Itinerario: {itinerario['tipoItinerario']} - Cantidad de aterrizajes: {itinerario['cantAterrizajes']}", 1, 1)
             self.cell(0, 10, '', 0, 1)  # Espacio entre itinerarios
+
 
     def generar_nombre_archivo(self, id_recibos):
     
@@ -75,38 +86,36 @@ class ReciboPDF(FPDF):
             fecha_recibo = recibo.fecha
 
             numRecibo = recibo.numero_recibos
-            pdf.datos_recibo(recibos[0], fecha_recibo)
+            pdf.datos_recibo(recibos[0], fecha_recibo,numRecibo)
             recibos.pop(0)
             pdf.itinerarios(recibos)
-            
-        # Guardar el PDF con el nombre de archivo generado
-            ##directorio_actual_normalizado = os.path.normpath(directorio_actual)
-            namePdf = f'recibo-{numRecibo}.pdf'
-            pdf.output(namePdf, 'F')
-            print(f"El recibo ha sido creado como '{namePdf}'")
 
-        #variables de entorno del email
-         # Carga las variables de entorno desde el archivo .env
+            namePdf = f'recibo-{numRecibo}.pdf'
+            # Obtener el contenido del PDF como bytes
+            pdf_bytes = pdf.output(dest="S").encode("latin1")
+            #variables de entorno del email
+            # Carga las variables de entorno desde el archivo .env
             load_dotenv()
 
-                # Ahora puedes acceder a las variables de entorno como si estuvieran definidas en el sistema
+            # Ahora puedes acceder a las variables de entorno como si estuvieran definidas en el sistema
             emailSmtp = os.getenv('EMAIL_SMTP')
             passwordSmtp = os.getenv('PASSWORD_SMTP')        
 
         # Envío por correo electrónico
-            msg = EmailMessage()
+            msg = MIMEMultipart()
             msg['Subject'] = 'Recibo'
             msg['From'] = emailSmtp  # Cambia esto al remitente real
-            msg['To'] = emailAsociado # pasarle el emailAsociado correspondiente 
-            msg.set_content(f'Adjunto encontrarás el recibo número {numRecibo}') #Mensaje del cuerpo de email
+            msg['To'] = emailAsociado # pasarle el emailAsociado correspondiente
+            msg.attach(MIMEText(f'Adjunto encontrarás el recibo número {numRecibo}', "plain")) 
+           #msg.set_content(f'Adjunto encontrarás el recibo número {numRecibo}') #Mensaje del cuerpo de email
 
             numRecibo = recibo.numero_recibos
 
-            with open(namePdf, 'rb') as f:   #Abre el archivo PDF en modo lectura binaria ('rb')
-                contenido_pdf = f.read()    #Lee el contenido del archivo PDF y lo guarda en la variable contenido_pdf.
-                nombre_pdf = os.path.basename(namePdf)   #Obtiene el nombre del archivo PDF sin la ruta completa.
-                msg.add_attachment(contenido_pdf, maintype='application', subtype='octet-stream', filename=nombre_pdf)      #Adjunta el contenido del archivo PDF al correo electrónico utilizando el método add_attachment.
-
+            # Adjuntar el PDF al mensaje de correo electrónico
+            # Convertir BytesIO a bytes
+            pdf_attachment = MIMEApplication(pdf_bytes, _subtype="pdf")
+            pdf_attachment.add_header('Content-Disposition', 'attachment', filename=namePdf)
+            msg.attach(pdf_attachment)
             # Configura el servidor SMTP y envía el correo
             servidor_smtp = 'smtp.gmail.com'  # Cambia esto según tu proveedor de correo
             puerto_smtp = 587  # Cambia el puerto si es diferente (puede ser 465)
@@ -118,25 +127,12 @@ class ReciboPDF(FPDF):
                 with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
                     servidor.starttls()       # inicia una conexión segura con el servidor SMTP mediante el protocolo TLS (Transport Layer Security)
                     servidor.login(usuario_smtp, password_smtp)
-                    servidor.send_message(msg)
+                    servidor.sendmail(usuario_smtp, emailAsociado, msg.as_string())
 
                 print(f"El recibo ha sido enviado por correo electrónico {emailAsociado}")
             except Exception as e:
                     print(f"Error al enviar el correo electrónico: {e}") 
-            
-            try:
-                # Obtiene la ruta absoluta del archivo actual (script)
-                ruta_actual = os.path.abspath(__file__)
 
-                # Retrocede dos niveles para llegar a la "carpeta raíz" del proyecto
-                ruta_pdf = os.path.abspath(os.path.join(ruta_actual,'..', '..',f'../{nombre_pdf}'))
-                os.remove(ruta_pdf)
-                print(f"Archivo {ruta_pdf} eliminado exitosamente.")
-            except FileNotFoundError:
-                print(f"El archivo {ruta_pdf} no existe.")
-            except Exception as e:
-                print(f"Ocurrió un error al intentar borrar el archivo: {e}")
-            
             return True
         
         except Exception as ex:
